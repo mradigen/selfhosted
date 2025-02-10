683a9b603cdeddda08eac83a841d763cbaecabe6
mradigen <55953083+mradigen@users.noreply.github.com>
2025-02-10 23:49:41 +0530

    add: modify update.sh

--- update.sh
+++ update.sh
@@ -9,27 +9,28 @@ git pull
 
 # Fetch global environment variables
 source .env
-stacks=$(ls -d */)
+stacks=$(ls -d */ | sed 's:/$::')
 
 update_env() {
-    local env_file=".env"
-    local var_name="$1"
-    local new_value="$2"
+	local ENV_FILE=".env"
+	local VAR_NAME="$1"
+	local NEW_VALUE="$2"
 
-    # Check if .env file exists
-    if [[ -f "$env_file" ]]; then
-        # Update variable if it exists, else append it
-        grep -q "^$var_name=" "$env_file" && sed -i '' -E "s/^$var_name=.*/$var_name=$new_value/" "$env_file" || echo "$var_name=$new_value" >> "$env_file"
-    else
-        echo "$env_file does not exist."
-    fi
+	if [[ -f "$ENV_FILE" ]]; then
+		if grep -q "^$VAR_NAME=" "$ENV_FILE"; then
+			sed -i "s|^$VAR_NAME=.*|$VAR_NAME=$NEW_VALUE|" "$ENV_FILE"
+		else
+			echo "$VAR_NAME=$NEW_VALUE" >> "$ENV_FILE"
+		fi
+	else
+		echo "$ENV_FILE does not exist."
+	fi
 }
 
 for stack in ${stacks}
 do
 	cd ${stack}
-	update_env DATA_PATH ${DATA_HOME}/${stack}
+	update_env DATA_PATH "${DATA_HOME}/${stack}"
 	DATA_PATH=${DATA_HOME}/${stack} docker compose up -d
 	cd ..
 done
-
