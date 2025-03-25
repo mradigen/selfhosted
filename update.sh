2ce0fb548ae78765de3168792fe9e58db3476643
mradigen <55953083+mradigen@users.noreply.github.com>
2025-03-25 09:53:55 +0530

    fix: hardcode stack names

--- update.sh
+++ update.sh
@@ -4,41 +4,41 @@
 set -e
 
 update_env() {
-	local ENV_FILE=".env"
-	local VAR_NAME="$1"
-	local NEW_VALUE="$2"
+        local ENV_FILE=".env"
+        local VAR_NAME="$1"
+        local NEW_VALUE="$2"
 
-	if grep -q "^$VAR_NAME=" "$ENV_FILE"; then
-		sed -i "s|^$VAR_NAME=.*|$VAR_NAME=$NEW_VALUE|" "$ENV_FILE"
-	else
-		echo "$VAR_NAME=$NEW_VALUE" >> "$ENV_FILE"
-	fi
+        if grep -q "^$VAR_NAME=" "$ENV_FILE"; then
+                sed -i "s|^$VAR_NAME=.*|$VAR_NAME=$NEW_VALUE|" "$ENV_FILE"
+        else
+                echo "$VAR_NAME=$NEW_VALUE" >> "$ENV_FILE"
+        fi
 }
 
 # Fetch global environment variables
 source .env
-stacks=$(ls -d */ | sed 's:/$::')
+stacks=(baikal balti-minio chota immich paperless-ng traefik vaultwarden balti borgmatic headscale kachra n8n)
 
 # Stop all services:
-for stack in ${stacks}
+for stack in ${stacks[@]}
 do
-	cd ${stack}
-	update_env DATA_PATH "${DATA_HOME}/${stack}"
-	docker compose down
-	cd ..
+        cd ${stack}
+        update_env DATA_PATH "${DATA_HOME}/${stack}"
+        docker compose down
+        cd ..
 done
 
 # Fetch latest
 git stash
 git pull
 
-stacks=$(ls -d */ | sed 's:/$::')
+stacks=(baikal balti-minio chota immich paperless-ng traefik vaultwarden balti borgmatic headscale kachra n8n)
 
 # Start them back up
-for stack in ${stacks}
+for stack in ${stacks[@]}
 do
-	cd ${stack}
-	update_env DATA_PATH "${DATA_HOME}/${stack}"
-	docker compose up -d
-	cd ..
+        cd ${stack}
+        update_env DATA_PATH "${DATA_HOME}/${stack}"
+        docker compose up -d
+        cd ..
 done
